1adec4f828210fde62ba2c014b326801c65da33c
Ricardo Matos <ricardothadeu8@outlook.com>
2022-11-03 19:10:44 -0300

    Add ACC_REG

--- acc_reg.v
+++ acc_reg.v
@@ -3,7 +3,6 @@ module acc_reg (
 	input reset, 
 	input acc_in,
 	input acc_out,
-	output reg[7:0] d,
 	output reg[7:0] q,
 	input[7:0] bus_in,
 	output reg[7:0] bus_out);
@@ -13,11 +12,10 @@ module acc_reg (
 		if(reset) 
 			q <= 0;
 		else begin
-			if(acc_in) begin 
-				d <= bus_in;
-				q <= d; end
-			
-			if(acc_out) bus_out <= q;
+			if(acc_in)
+				q <= bus_in;			
+			if(acc_out) 
+				bus_out <= q;
 		end
 	end
 		
