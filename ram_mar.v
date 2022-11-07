78b1106e4466800065295b154af1913539b380dc
Ricardo Matos <ricardothadeu8@outlook.com>
2022-11-06 23:03:28 -0300

    Add RAM
    RAM testada após o uso do BUS bidirecional. Testes feitos, que mostraram
    as etapas de leitura e escrita funcionando como esperado.

--- ram_mar.v
+++ ram_mar.v
@@ -6,23 +6,26 @@ module ram_mar (
 	input ram_out,
 	output reg[3:0] mar_addr, //vem do MAR. BUS[3:0]
 	//input[7:0] write_mem, //vem do BUS 
-	input[7:0] bus,
-	output reg[7:0] bus_out);
+	//input[7:0] bus_in,
+	//output reg[7:0] bus_out);
+	
+	inout reg[7:0] bus);
 	
 	reg [7:0]mem[15:0];
-
+	
 	always @(posedge clock) begin
-		if(reset) mar_addr <= 0;
+		if(reset) mar_addr <= 4'b0;
 		else begin
 			if(mar_in)
 				mar_addr <= bus[3:0];
+			else bus <= 8'bz;
 		end
 	end
 	
 	
 	always @(posedge clock) begin
 		if(ram_in) mem[mar_addr] <= bus; //escrita
-		if(ram_out) bus_out <= mem[mar_addr]; //leitura
+		if(ram_out) bus <= mem[mar_addr]; else bus <= 8'bz;//leitura
 	end
 	
 endmodule
